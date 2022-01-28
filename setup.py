8ddc99a1c19bbe65026b4f40b5cea944dbcafecf
Marcus Hughes <hughes.jmb@gmail.com>
2022-01-27 21:04:10 -0700

    added setup.py and LICENSE.mds

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='slidingpuzzle',
+    version='0.0.1',
+    packages=['slidingpuzzle', 'slidingpuzzle.tests'],
+    url='https://slidingpuzzle.readthedocs.io/en/latest/?',
+    license='MIT',
+    author='jmbhughes',
+    author_email='hughes.jmb@gmail.com',
+    description='a sliding puzzle solver'
+)
